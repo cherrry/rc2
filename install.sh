eb7122ed4011909d3ffde77ff68d6fa2562bed7b
Cherry Ng <cherry92@gmail.com>
2020-01-28 00:23:22 +0800

    Add xfce4-terminal related settings.

--- install.sh
+++ install.sh
@@ -6,3 +6,16 @@ cd "${RC_BASE}"
 # Necessary submodules
 git submodule update --init --recursive
 git submodule update --remote --recursive
+
+# Linux specific
+if [ `uname` == 'Linux' ]; then
+  LINUX_BASE="${RC_BASE}/linux"
+  # xfce4
+  mkdir -p "${HOME}/.config/xfce4/terminal"
+  rm -f "${HOME}/.config/xfce4/terminal/terminalrc"
+  ln -s "${LINUX_BASE}/xfce4/terminal/terminalrc" "${HOME}/.config/xfce4/terminal/terminalrc"
+  # gtk-3.0
+  mkdir -p "${HOME}/.config/gtk-3.0"
+  rm -f "${HOME}/.config/gtk-3.0/gtk.css"
+  ln -s "${LINUX_BASE}/gtk-3.0/gtk.css" "${HOME}/.config/gtk-3.0/gtk.css"
+fi
