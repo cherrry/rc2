75113d25344c9e6a505da0b601d6af072e9cf56b
Cherry Ng <cherry92@gmail.com>
2020-03-12 09:57:37 +0800

    Replace i3status with i3pystatus.

---
+++ linux/i3/i3status/pystatus.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from i3pystatus import Status
+
+
+def main():
+    # Register from right to left.
+    status = Status()
+    status.register('clock', format=('🌍 %H:%M', 'Europe/Dublin'))
+    status.register('clock',
+                    format=(' / %H:%M', 'America/New_York'),
+                    hints={'separator': False})
+    status.register('clock',
+                    format=('🌎 %H:%M', 'America/Los_Angeles'),
+                    hints={
+                        'separator': False,
+                        'separator_block_width': 0
+                    })
+    status.register('clock',
+                    format=('🌏 HKG %H:%M', 'Asia/Hong_Kong'),
+                    hints={'separator': False})
+    status.register('shell', command='~/.config/i3/i3status/today.py')
+
+    status.register('battery',
+                    format='{status} {percentage:.1f}%',
+                    status={
+                        'CHR': '🔌',
+                        'DIS': '🔋',
+                        'DPL': 'DPL',
+                        'FULL': '⚡',
+                    })
+    status.register('pulseaudio',
+                    format='🔉 {volume}%',
+                    format_muted='🔇 Muted',
+                    hints={'separator': False},
+                    on_leftclick='switch_mute')
+    status.run()
+
+
+if __name__ == '__main__':
+    main()
